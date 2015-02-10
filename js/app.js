d123e8ada085293ab7d56856dd80a551dbc8dcac
spaetsch <spaetsch@gmail.com>
2015-02-10 13:55:54 -0800

    Create structure for Player and Enemy classes. Create allEnemies array.

--- js/app.js
+++ js/app.js
@@ -24,12 +24,43 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our player, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-horn-girl.png';
+}
+
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+}
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(){
+
+    //??
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+var enemy1 = new Enemy;
+var enemy2 = new Enemy;
+var enemy3 = new Enemy;
 
+var allEnemies=[enemy1, enemy2, enemy3];
+
+
+// Place the player object in a variable called player
+var player = new Player;
 
 
 // This listens for key presses and sends the keys to your
