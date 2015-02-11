3bc1ee51acecfb6b9e3b5403424ae215641986f8
spaetsch <spaetsch@gmail.com>
2015-02-10 16:53:34 -0800

    Add bug movement. Has randomized speeds and bounds checking.

--- js/app.js
+++ js/app.js
@@ -7,18 +7,8 @@ var Enemy = function() {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 
-    //bug origin - upper left stone square (0, 60)
-    //randomize position max 500 -> x coord
-    //randomize lane 0, 1, 2 -> y-coord
-
-    var lane = Math.floor(Math.random() * 2);
-    var position = Math.floor(Math.random() * 500);
-    this.x = 0 + position; //incremental crawling to the right
-    this.y = 60 + lane * 80; // one of the three lanes 0, 1, 2
-
-    console.log("randomize enemy location");
-    console.log("x =", this.x);
-    console.log("y =", this.y);
+    //Sets initial position, speed
+    this.reset();
 }
 
 // Update the enemy's position, required method for game
@@ -27,11 +17,33 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    this.x = this.x + this.speed *dt;
+
+    //if the enemy goes off the screen, re-initialize with new speed and y-coord
+    // set x-coord to offscreen left
+    if (this.x > 500){
+        this.reset();
+        this.x = -50;
+    }
 }
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Enemy.prototype.reset = function(){
+
+    //bug origin - upper left stone square (0, 60)
+
+    var lane = Math.floor(Math.random() * 2);
+    var position = Math.floor(Math.random() * 500);
+    this.x = 0 + position; //incremental crawling to the right
+    this.y = 60 + lane * 80; // one of the three lanes 0, 1, 2
+    this.speed = 10 + Math.random()*200;
+
+
 }
 
 // Now write your own player class
@@ -44,10 +56,8 @@ var Player = function() {
     // The image/sprite for our player, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/char-horn-girl.png';
-
-    //initial location
-   // this.x = 200;// +/- 100;
-    //this.y = 380; //- 80;
+ 
+    // set to initial location 
     this.reset();
 }
 
@@ -55,6 +65,7 @@ Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
 }
 
 // Draw the enemy on the screen, required method for game
@@ -64,38 +75,37 @@ Player.prototype.render = function() {
 
 Player.prototype.handleInput = function(allowedKeys){
     if (allowedKeys === 'left'){
-        if (this.x < 50){
-            this.x=this.x;
-        } else {
+        //if not in leftmost square, move left by one tile
+        if (this.x > 50){
             this.x = this.x - 101;
         }
     }
     if (allowedKeys === 'right'){
-        if (this.x > 400){
-            this.x=this.x;
-        } else {
-        this.x = this.x + 101;
-        }
+        //if not in the rightmost square, move right by one tile
+        if (this.x < 400){
+            this.x = this.x + 101;
+        } 
     }
     if (allowedKeys === 'up'){
+        //if in the top row, move to initial position
         if (this.y < 80){
             this.reset();
         } else {
-        this.y = this.y - 83;
+            //if not in the top row, move up one row
+            this.y = this.y - 83;
         }
     }
     if (allowedKeys === 'down'){
-        if (this.y > 300){
-            this.y =this.y;
-        } else {
+        //if not in the bottom row, move down one row
+        if (this.y < 300){
             this.y = this.y + 83;
-        }
+        } 
     }
 
 }
 
 Player.prototype.reset = function(){
-     //initial location
+    //initial Player location
     this.x = 200;
     this.y = 380; 
 }
@@ -108,10 +118,8 @@ var enemy3 = new Enemy;
 
 var allEnemies=[enemy1, enemy2, enemy3];
 
-
 // Place the player object in a variable called player
 var player = new Player;
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
